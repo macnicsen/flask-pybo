21794d9c7be8c0ab8b2a69980e5f015bb087ec8c
김도암 <do9318@hanmail.net>
2022-08-05 11:58:54 +0900

    user_id 변경

--- pybo/models.py
+++ pybo/models.py
@@ -20,7 +20,7 @@ class Question(db.Model):
     content =db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(),nullable=False)
     # 작성자
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=True,server_default='1')
     user = db.relationship('User', backref=db.backref('question_set'))
     # 모델수정
     modify_date = db.Column(db.DateTime(), nullable=True)
@@ -34,7 +34,7 @@ class Answer (db.Model):
     question = db.relationship('Question', backref=db.backref('answer_set'))
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=True,server_default='1')
     user = db.relationship('User', backref=db.backref('answer_set'))
     # 모델수정
     modify_date = db.Column(db.DateTime(), nullable=True)
